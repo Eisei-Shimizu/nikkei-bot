9041d236a4f7626e867169b9aeb1fda85ecc90db
Eisei Shimizu <eisei37411031@gmail.com>
2020-09-12 00:16:26 +0900

    先物のクローズ値の一覧を取得できるようにした

---
+++ nikkei.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async getNikkei1hourCharts() {
+    var apiKey = "b70d850f62msh14f5fe645adb47fp113163jsn9694232e036d";
+    var request = require("request-promise");
+
+    var options = {
+      method: "GET",
+      url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts",
+      qs: { region: "JP", symbol: "NIY%3DF", interval: "60m", range: "3mo" },
+      headers: {
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        "x-rapidapi-key": apiKey,
+        useQueryString: true,
+      },
+    };
+
+    var result = request(options)
+      .then(function (body) {
+        console.log(body);
+        return JSON.parse(body);
+      })
+      .catch(function (err) {
+        console.log(err);
+        return err;
+      });
+
+    return result;
+  },
+};
--- skelton.js
+++ skelton.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-
+const nikkei = require("./nikkei.js");
 (async () => {
   // Puppeteerの起動
   const browser = await puppeteer.launch({
@@ -16,6 +16,16 @@ const puppeteer = require("puppeteer");
   });
 
   await page.goto("http://www.google.co.jp/");
+
+  var result = await nikkei.getNikkei1hourCharts();
+  if (!result["chart"]["error"]) {
+    console.log("<<<<<<<<<<<<<<<<<");
+    console.log(result["chart"]["result"][0]["indicators"]["quote"][0].close);
+
+    // 平均線を3, 10, 25 それぞれで算出
+  } else {
+    console.log(result["chart"]["error"]);
+  }
 
   // ブラウザ終了
   await browser.close();
