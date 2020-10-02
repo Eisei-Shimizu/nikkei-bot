b6780b3a5646ced1f53ea7bf2deab0726c9141b3
Eisei Shimizu <eisei37411031@gmail.com>
2020-10-03 02:44:09 +0900

    ループして画面を更新するようにした

--- skelton.js
+++ skelton.js
@@ -76,117 +76,122 @@ async function trade() {
 
   await tradePage.waitFor(10000);
 
-  // ポジションがあるかどうか決済ボタンの表示で判断
-  const position = await tradePage.$$('button[cellbutton="true"]');
-  let posSide = SIDE_NONE;
+  // 取引可能時間内でループ
+  while (checkTradeTime()) {
+    // ポジションがあるかどうか決済ボタンの表示で判断
+    const position = await tradePage.$$('button[cellbutton="true"]');
+    let posSide = SIDE_NONE;
 
-  if (position.length != 0) {
-    // 買い or 売りチェック
-    const posSideText = await tradePage.evaluate(
-      () =>
-        document.querySelector("tr.datagrid-row > td:nth-child(3) > div > span")
-          .textContent
-    );
+    if (position.length != 0) {
+      // 買い or 売りチェック
+      const posSideText = await tradePage.evaluate(
+        () =>
+          document.querySelector(
+            "tr.datagrid-row > td:nth-child(3) > div > span"
+          ).textContent
+      );
 
-    if (posSideText == POS_SIDE_BUY) {
-      posSide = SIDE_BUY;
-    } else if (posSideText == POS_SIDE_SELL) {
-      posSide = SIDE_SELL;
-    }
-
-    // ロスカットチェック
-    // ロスカット後は建玉画面に遷移
-    let currentPriceText = await tradePage.evaluate(
-      () =>
-        document.querySelector("tr.datagrid-row > td:nth-child(7) > div > div")
-          .textContent
-    );
-
-    let posPriceText = await tradePage.evaluate(
-      () =>
-        document.querySelector("tr.datagrid-row > td:nth-child(6) > div")
-          .textContent
-    );
-
-    currentPriceText = currentPriceText.split(" ")[0].replace(",", "");
-    posPriceText = posPriceText.split(" ")[0].replace(",", "");
-
-    console.log("評価額: " + currentPriceText);
-    console.log("建て値: " + posPriceText);
-
-    const currentPrice = parseInt(currentPriceText);
-    const posPrice = parseInt(posPriceText);
-    const lossCutRange = setting["lossCutRange"];
-
-    let isLossCut = false;
-    if (posSide == SIDE_BUY && -lossCutRange >= currentPrice - posPrice) {
-      isLossCut = true;
-    } else if (
-      posSide == SIDE_SELL &&
-      lossCutRange >= currentPrice - posPrice
-    ) {
-      isLossCut = true;
-    }
-
-    if (isLossCut) {
-      // 精算
-      console.log("ロスカット");
-      gotoPositionView(tradePage);
-      liquidation(tradePage);
-    }
-  }
-
-  // TODO: 取引時間チェック
-
-  var result = await nikkei.getNikkei1hourCharts();
-  if (!result["chart"]["error"]) {
-    const closePriceList =
-      result["chart"]["result"][0]["indicators"]["quote"][0].close;
-    const filterdClosePriceList = closePriceList.filter(function (value) {
-      return value != null;
-    });
-
-    const smaPeriodList = [3, 10, 25];
-
-    // 平均線を3, 10, 25 それぞれで算出
-    const sma = getSMA(filterdClosePriceList, smaPeriodList);
-    console.log(sma);
-
-    // G.C or D.C
-    const isCross = sma[0] == sma[1];
-
-    if (isCross) {
-      // 乖離幅設定
-      const deviationRange = setting["deviationRange"];
-
-      // 注文
-      let orderSide = SIDE_NONE;
-
-      // 短平均線が中平均線上抜け =>　買い
-      if (deviationRange >= sma[0] - sma[1]) {
-        // 買い注文
-        orderSide = SIDE_BUY;
-      }
-      // 短平均線が中平均線下抜け =>　売り
-      else if (deviationRange >= sma[1] - sma[0]) {
-        // 売り注文
-        orderSide = SIDE_SELL;
+      if (posSideText == POS_SIDE_BUY) {
+        posSide = SIDE_BUY;
+      } else if (posSideText == POS_SIDE_SELL) {
+        posSide = SIDE_SELL;
       }
 
-      if (posSide == SIDE_NONE && orderSide != SIDE_NONE) {
-        // エントリー
-        order(tradePage, orderSide);
-      } else if (posSide != SIDE_NONE && posSide != orderSide) {
+      // ロスカットチェック
+      // ロスカット後は建玉画面に遷移
+      let currentPriceText = await tradePage.evaluate(
+        () =>
+          document.querySelector(
+            "tr.datagrid-row > td:nth-child(7) > div > div"
+          ).textContent
+      );
+
+      let posPriceText = await tradePage.evaluate(
+        () =>
+          document.querySelector("tr.datagrid-row > td:nth-child(6) > div")
+            .textContent
+      );
+
+      currentPriceText = currentPriceText.split(" ")[0].replace(",", "");
+      posPriceText = posPriceText.split(" ")[0].replace(",", "");
+
+      console.log("評価額: " + currentPriceText);
+      console.log("建て値: " + posPriceText);
+
+      const currentPrice = parseInt(currentPriceText);
+      const posPrice = parseInt(posPriceText);
+      const lossCutRange = setting["lossCutRange"];
+
+      let isLossCut = false;
+      if (posSide == SIDE_BUY && -lossCutRange >= currentPrice - posPrice) {
+        isLossCut = true;
+      } else if (
+        posSide == SIDE_SELL &&
+        lossCutRange >= currentPrice - posPrice
+      ) {
+        isLossCut = true;
+      }
+
+      if (isLossCut) {
         // 精算
+        console.log("ロスカット");
+        gotoPositionView(tradePage);
         liquidation(tradePage);
       }
     }
-  } else {
-    console.log(result["chart"]["error"]);
+
+    var result = await nikkei.getNikkei1hourCharts();
+    if (!result["chart"]["error"]) {
+      const closePriceList =
+        result["chart"]["result"][0]["indicators"]["quote"][0].close;
+      const filterdClosePriceList = closePriceList.filter(function (value) {
+        return value != null;
+      });
+
+      const smaPeriodList = [3, 10, 25];
+
+      // 平均線を3, 10, 25 それぞれで算出
+      const sma = getSMA(filterdClosePriceList, smaPeriodList);
+      console.log(sma);
+
+      // G.C or D.C
+      const isCross = sma[0] == sma[1];
+
+      if (isCross) {
+        // 乖離幅設定
+        const deviationRange = setting["deviationRange"];
+
+        // 注文
+        let orderSide = SIDE_NONE;
+
+        // 短平均線が中平均線上抜け =>　買い
+        if (deviationRange >= sma[0] - sma[1]) {
+          // 買い注文
+          orderSide = SIDE_BUY;
+        }
+        // 短平均線が中平均線下抜け =>　売り
+        else if (deviationRange >= sma[1] - sma[0]) {
+          // 売り注文
+          orderSide = SIDE_SELL;
+        }
+
+        if (posSide == SIDE_NONE && orderSide != SIDE_NONE) {
+          // エントリー
+          order(tradePage, orderSide);
+        } else if (posSide != SIDE_NONE && posSide != orderSide) {
+          // 精算
+          liquidation(tradePage);
+        }
+      }
+    } else {
+      console.log(result["chart"]["error"]);
+    }
+
+    RegularlyPageReload(tradePage);
   }
 
   // ブラウザ終了
-  //await browser.close();
+  await browser.close();
 }
 
 function convertWeekdays(day) {
@@ -385,7 +390,11 @@ async function liquidation(page) {
 }
 
 async function RegularlyPageReload(page) {
-  await page.evaluate(() => {
-    location.reload(true);
-  });
+  await Promise.all([
+    page.evaluate(() => {
+      location.reload(true);
+    }),
+  ]);
+
+  await page.waitFor(5000);
 }
