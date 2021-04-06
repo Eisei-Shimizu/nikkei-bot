5129d78826337756dab0df60748bda6f2c2b5489
Eisei Shimizu <eisei37411031@gmail.com>
2021-04-06 12:11:30 +0900

    コードフォーマット、セル取得位置の修正

--- load-charts.js
+++ load-charts.js
@@ -1,26 +1,25 @@
 module.exports = {
-    loadCharts(){
-        const fs = require("fs");
-        const setting = JSON.parse(fs.readFileSync("./setting.json", "utf8"));
-        const xlsx = require('xlsx');
-        const MAX_CHARTS_LEN = 25;
-        const FIRST_CEL_POS = 3;
+  loadCharts() {
+    const fs = require("fs");
+    const setting = JSON.parse(fs.readFileSync("./setting.json", "utf8"));
+    const xlsx = require("xlsx");
+    const MAX_CHARTS_LEN = 25;
+    const FIRST_CEL_POS = 4;
 
-        try{
-            let chartsFile = xlsx.readFile(setting["chartsFilePath"]);
-            let worksheet = chartsFile.Sheets['四本値'];
-            let closePriceList = [];
+    try {
+      let chartsFile = xlsx.readFile(setting["chartsFilePath"]);
+      let worksheet = chartsFile.Sheets["四本値"];
+      let closePriceList = [];
 
-            for(var i=0; i < MAX_CHARTS_LEN; i++){
-                closePriceList.push(worksheet['G' + (FIRST_CEL_POS + i)].v);
-                console.log(closePriceList[i]);
-            }
+      for (var i = 0; i < MAX_CHARTS_LEN; i++) {
+        closePriceList.push(worksheet["G" + FIRST_CEL_POS].v);
+        console.log(closePriceList[i]);
+      }
 
-            return closePriceList;
-        }
-        catch(err){
-            console.log(err);
-            return [];
-        }
+      return closePriceList;
+    } catch (err) {
+      console.log(err);
+      return [];
     }
-}
+  },
+};
